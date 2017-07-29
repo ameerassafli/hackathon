4a9bb01c6b2373c9f5e436912b2fc68e14332e20
ameerassafli <amiralassafi@yahoo.com>
2017-07-29 03:33:40 +0300

    first

--- app.js
+++ app.js
@@ -1,57 +1,96 @@
 const mostCommon = require('most-common');
-//const inArray = require('in-array');
 const _ = require('underscore');
-var columns = ["Date_and_Time", "Caller_ID", "Employee_ID" , "Duration", "Talk_Time","Status"];
+var columns = ["Date_and_Time", "Caller_ID", "Employee_ID", "Duration", "Talk_Time", "Status"];
 var prob1 = [];
+var prob3 = [];
+var prob35 = [];
 var prob4 = [];
 var prob7 = [];
 var prob6 = [];
-var w ;
- require("csv-to-array")({
-   file: "./data.csv",
-   columns: columns
-},  function (err, array) {
-  //console.log(err || array);
- 
-  for(let i=0 ; i<array.length ; i++){
-prob1.push(array[i].Date_and_Time);
-prob1[i]= prob1[i].slice(0,16);
-if(array[i].Status == "ANSWERED"){
-prob4.push(  array[i].Employee_ID  );
-// let id = array[i].Caller_ID;
-// let dur = parseInt(array[i].Talk_Time) ;
-// //console.log(id);
-// //console.log(_.indexOf(prob6 , {id : id })!= -1)
-// if (_.findLastIndex(prob6 , {  id : id })!= -1){
-// 	 w= _.findLastIndex(prob6 , {  id: id});
-// 	// console.log(w)
-// 	prob6[w].dur +=dur ;
-	
 
-// }
+require("csv-to-array")({
+    file: "./data.csv",
+    columns: columns
+}, function(err, array) {
+   
 
-// else {prob6.push({'id' : id , 'dur': dur}); }
+    for (let i = 0; i < array.length; i++) {
+        prob1.push(array[i].Date_and_Time);
+        prob3.push(JSON.stringify({'Caller_ID':array[i].Caller_ID , 'Employee_ID' : array[i].Employee_ID}))
+        prob35.push({ 'ce': JSON.stringify({'Caller_ID':array[i].Caller_ID , 'Employee_ID' : array[i].Employee_ID}) , 'dur': parseInt(array[i].Talk_Time)})
+
+        prob1[i] = prob1[i].slice(0, 16);
+        if (array[i].Status == "ANSWERED") {
+            prob4.push(array[i].Employee_ID);
+
+            let dur = parseInt(array[i].Talk_Time);
+            prob6.push({
+                'id': array[i].Caller_ID,
+                'dur': dur
+            })
+        }
+        prob7.push(array[i].Caller_ID);
+
+    }
 
 
 
-}
-prob7.push(array[i].Caller_ID);
-//console.log(peak[i]);
-}
+    prob6.sort(function(a, b) {
+        let probA = a.id,
+            probB = b.id
+        if (probA < probB)
+            return -1
+        if (probA > probB)
+            return 1
+        return 0
+    })
+    for (let i = 0; i < prob6.length - 1; i++) {
+        if (prob6[i].id == prob6[i + 1].id) {
+            prob6[i + 1].dur += prob6[i].dur;
+        } else {
+            continue;
+        }
+    }
 
-//console.log(prob6.length)
-let ans = mostCommon(prob1, 1);
-console.log("Peak minute of incoming phonecalls. (including not answered phonecall): "+ans[0].token);
-ans = mostCommon(prob4, 1);
-console.log("Most productive employee (employee with most answered phonecalls): " + ans[0].token);
-var x = []
-x = mostCommon(prob4 , prob4.length);
-console.log ( "Least productive employee (employee with least answered phonecalls): "+ x[x.length - 2].token);
-x = [];
-ans = mostCommon(prob7 , 1);
-console.log( "Client with most frequent calls: "+ ans[0].token);
-//console.log(_.max(prob6, function(prob){ return prob.dur; }));
+prob35.sort(function(a, b) {
+        let probA = a.ce,
+            probB = b.ce
+        if (probA < probB)
+            return -1
+        if (probA > probB)
+            return 1
+        return 0
+    })
+    for (let i = 0; i < prob6.length - 1; i++) {
+        if (prob35[i].ce == prob35[i + 1].ce) {
+            prob35[i + 1].dur += prob35[i].dur;
+        } else {
+            continue;
+        }
+    }
+
+    let ans = mostCommon(prob1, 1);
+    console.log("Peak minute of incoming phonecalls. (including not answered phonecall):\n " + ans[0].token + "\n");
+    ans2 = mostCommon (prob3 , 1);
+    console.log("Suspicious Relations : ")
+    console.log("1 - Most Calls: " + ans2[0].token);
+    console.log("2 - Longest Talk Time: ")
+    console.log( _.max(prob35, function(prob) {
+        return prob.dur;
+    }));
+    ans = mostCommon(prob4, 1);
+    console.log("Most productive employee (employee with most answered phonecalls): " + ans[0].token + "\n");
+    var x = []
+    x = mostCommon(prob4, prob4.length);
+    console.log("Least productive employee (employee with least answered phonecalls): " + x[x.length - 2].token + "\n");
+    x = [];
+    console.log("Client with longest talk time: ")
+    console.log( _.max(prob6, function(prob) {
+        return prob.dur;
+    }));
+    ans = mostCommon(prob7, 1);
+    console.log("Client with most frequent calls: " + ans[0].token + "\n");
+
 
 
 });
-
